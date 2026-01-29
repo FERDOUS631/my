7115b2a2c8c373321cc8cfb741e47c091626090e
Ferdous Reza <ferdousreza631@gmail.com>
2026-01-29 20:08:55 +0600

    commit

--- kpi_notice/settings.py
+++ kpi_notice/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-pj)ti5o27(=)3ant8w3vw003y^dak7w)caw)9x_1tlt#$!uml9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['kpinoticeboard.up.railway.app', 'localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
